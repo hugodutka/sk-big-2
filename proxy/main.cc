f45d3c80300f4528800f7b930c9d2fe74cba56cb
Hugo Dutka <dutkahugo@gmail.com>
2020-05-30 17:56:49 +0200

    add exception handling

--- proxy/main.cc
+++ proxy/main.cc
@@ -7,17 +7,28 @@
 using namespace std;
 
 int main() {
-  shared_ptr<Broadcaster> broadcaster = make_shared<StdoutBroadcaster>();
-  ICYStream stream = ICYStream("waw02-03.ic.smcdn.pl", "/t050-1.mp3", 8000, 5, true);
-  stream.open_stream();
+  try {
+    shared_ptr<Broadcaster> broadcaster = make_shared<StdoutBroadcaster>();
+    ICYStream stream = ICYStream("waw02-03.ic.smcdn.pl", "/t050-1.mp3", 8000, 5, true);
+    stream.open_stream();
 
-  size_t buf_size = stream.get_chunk_size();
-  shared_ptr<u8[]> buf(new u8[buf_size]);
+    size_t buf_size = stream.get_chunk_size();
+    shared_ptr<u8[]> buf(new u8[buf_size]);
 
-  for (u64 i = 0; i < 10; i++) {
-    ICYPart part = stream.read_chunk(buf.get());
-    broadcaster->broadcast(part, buf.get());
+    for (u64 i = 0; i < 100; i++) {
+      ICYPart part = stream.read_chunk(buf.get());
+      broadcaster->broadcast(part, buf.get());
+    }
+
+    stream.close_stream();
+    return 0;
+  } catch (exception& e) {
+    cerr << "An unexpected error occurred:" << endl;
+    cerr << e.what() << endl;
+    cerr << "Quitting." << endl;
+    return 1;
+  } catch (...) {
+    cerr << "An unexpected error occurred. Quitting." << endl;
+    return 1;
   }
-
-  stream.close_stream();
 }
